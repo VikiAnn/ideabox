0facb13e6b28646dd87bda6019faea9a6edac466
Viki Harrod <victoria.harrod@gmail.com>
2014-10-29 21:58:32 -0600

    wip testing

---
+++ test/app_test.rb
@@ -0,0 +1,28 @@
+require_relative 'test_helper'
+
+class AppTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    IdeaBoxApp
+  end
+
+  def test_index_page_contains_correct_header
+    response = get '/'
+    assert response.ok?
+    home_page = Nokogiri::HTML(response.body)
+    assert_includes home_page.content, "IdeaBox"
+  end
+
+  def test_index_page_contains_existing_ideas
+    response = get '/'
+    home_page = Nokogiri::HTML(response.body)
+    assert_includes home_page.content, "Existing Ideas"
+  end
+
+  def test_edit_page_loads
+    # this is a shitty test; i'm using actual database data
+    # which means it can reasonably be expected to change
+    assert get('/1/edit').ok?
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,9 @@
+$:.unshift File.expand_path("./../../lib", __FILE__)
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'rack/test'
 require 'simplecov'
+require 'nokogiri'
+require 'app'
+
 SimpleCov.start
